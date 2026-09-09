e091c13bec783a3b216a7c584774517047490c7f
jacob <jacobdhartzell@gmail.com>
2026-09-09 09:16:55 -0400

    completionish

---
+++ MaxValue.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class MaxValue {
+    public static void main(String[] args) {    
+        System.out.print("Enter in an integer:");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int count = 0;
+        int max = number;
+        
+        if (number == 0){
+            System.out.println("No numbers were entered except 0.");
+            input.close();
+            return;
+        }
+        while (max != 0){
+            number = input.nextInt();
+            if (number == 0 ){
+                break;
+            }
+            else if (max < number){
+                max = number;
+                count = 1;
+            }
+            else if (max == number){
+                count++;
+            }
+        }
+        System.out.print("The largest number is " + max + " and it occurs " + count + " times.");
+        input.close();
+    }
+}
